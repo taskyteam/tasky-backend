117a012c20ecdca3b7cd5352ad2f01a8a8deaac0
audunlk <audunlk@gmail.com>
2023-02-08 10:00:44 +0100

    added endpoints

--- server.js
+++ server.js
@@ -1,57 +1,60 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const jwt = require('jsonwebtoken');
-const database = require('./services/database');
-
-
+const jwt = require("jsonwebtoken");
+const database = require("./services/database");
 
 const PORT = process.env.PORT || 3333;
-const APP_SECRET = "legg til i .env"
+const APP_SECRET = "legg til i .env";
 
 app.use(cors());
 app.use(express.json());
 
-
-app.get('/tasks/households/:household_id', async (req, res) => {
-    const { household_id } = req.params;
-    const tasks = await database.getTasksByHousehold(household_id);
-    res.json(tasks);
+app.get("/tasks/households/:household_id", async (req, res) => {
+  const { household_id } = req.params;
+  const tasks = await database.getTasksByHousehold(household_id);
+  res.json(tasks);
 });
 
-app.get('/tasks/:user_id', async (req, res) => {
-    const { user_id } = req.params;
-    const tasks = await database.getTasksByUser(user_id);
-    res.json(tasks);
+app.get("/tasks/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+  const tasks = await database.getTasksByUser(user_id);
+  res.json(tasks);
 });
- 
- 
-app.get('/tasks/users/:household_id', async (req, res) => {
-    const { household_id } = req.params;
-    const tasks = await database.getUsersByHousehold(household_id);
-    res.json(tasks);
+
+app.get("/tasks/users/:household_id", async (req, res) => {
+  const { household_id } = req.params;
+  const tasks = await database.getUsersByHousehold(household_id);
+  res.json(tasks);
 });
- 
-app.post('/tasks', async (req, res) => {
-    const { title, description, assigned_to, points, status, household_id } = req.body;
-    const task = await database.createTask(title, description, assigned_to, points, status, household_id);
-    res.json(task);
-});
+
+app.post("/tasks", async (req, res) => {
+  const { title, description, points, assigned_to, household_id } = req.body;
+  const task = await database.createTask(
+    title,
+    description,
+    points,
+    assigned_to,
+    household_id
+  ); 
   
-app.put('/tasks/:id', async (req, res) => {
-    const { id } = req.params;
-    const { status } = req.body;
-    const task = await database.updateTask(id, status);
-    res.json(task);
+  res.json(task);
 });
 
-app.delete('/tasks/:id', async (req, res) => {
-    const { id } = req.params;
-    const task = await database.deleteTask(id);
-    res.json(task);
+app.put("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+  const task = await database.updateTask(id, status);
+  res.json(task);
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  const task = await database.deleteTask(id);
+  res.json(task);
 });
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port: ${PORT}!`);
+  console.log(`Example app listening on port: ${PORT}!`);
 });
--- services/database.js
+++ services/database.js
@@ -1,14 +1,15 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-const POSTGRES_URL = process.env.POSTGRES_URL // 'postgres://postgres:<ditt passord>@localhost:5432/<ditt database navn>';
+const POSTGRES_URL = process.env.POSTGRES_URL; // 'postgres://postgres:<ditt passord>@localhost:5432/<ditt database navn>';
 
 const database = new Pool({
-    connectionString: POSTGRES_URL,
+  connectionString: POSTGRES_URL,
 });
 // A database for an house chore app called Tasky
 
 async function getTasksByHousehold(household_id) {
-    const result = await database.query(`
+  const result = await database.query(
+    `
         SELECT 
             *
         FROM
@@ -16,14 +17,17 @@ async function getTasksByHousehold(household_id) {
         JOIN households ON tasks.household_id = households.id
         WHERE households.id = $1
         ORDER BY tasks.id DESC;
-    `, [household_id]);
-    
-    console.log(result.rows);
-    return result.rows;
+    `,
+    [household_id]
+  );
+
+  console.log(result.rows);
+  return result.rows;
 }
 
 async function getTasksByUser(user_id) {
-    const result = await database.query(`
+  const result = await database.query(
+    `
         SELECT
             *
         FROM
@@ -31,85 +35,98 @@ async function getTasksByUser(user_id) {
         WHERE
             tasks.assigned_to = $1
         ORDER BY tasks.id DESC;
-    `, [user_id]);
-    console.log(result.rows)
-    return result.rows;
+    `,
+    [user_id]
+  );
+  console.log(result.rows);
+  return result.rows;
 }
 
+async function createTask(
+  title,
+  description,
+  assigned_to,
+  points,
+  status,
+  household_id
+) {
+  // try{
+  // const userResult = await database.query(`
+  //     SELECT
+  //         users.id
+  //     FROM
+  //         users
+  //     WHERE
+  //         users.id = $1
+  // `, [assigned_to]);
+  // const assigned_user = userResult.rows[0];
+  // if(!assigned_user) throw new Error('User does not exist');
+  // } catch (error) {
+  //     console.log(error);
+  // }
+  console.log(points + "points");
+  points = parseInt(points);
+  console.log(points + "points parsed");
 
-async function createTask(title, description, assigned_to, points, status, household_id ) {
-    // try{
-    // const userResult = await database.query(`
-    //     SELECT
-    //         users.id
-    //     FROM 
-    //         users
-    //     WHERE
-    //         users.id = $1
-    // `, [assigned_to]);
-    // const assigned_user = userResult.rows[0];
-    // if(!assigned_user) throw new Error('User does not exist');
-    // } catch (error) {
-    //     console.log(error);
-    // }
-    console.log(points + "points")
-
-    points = parseInt(points);
-    console.log(points + "points parsed")
-    const result = await database.query(`
-        INSERT INTO tasks (title, description, assigned_to, status, points, household_id)
-        VALUES ($1, $2, $3, $4, $5, $6)
+  const result = await database.query(
+    `
+    INSERT INTO tasks (household_id, title, description, assigned_to, status, points)        VALUES ($1, $2, $3, $4, $5, $6)
         RETURNING *;
-    `, [ title, description, assigned_to, status, points, household_id ]);
-    console.log(result.rows[0]);
-    return result.rows[0];
+       `,
+    [household_id, title, description, assigned_to, status, points]
+  );
+  console.log(result.rows);
+  return result.rows;
 }
 
 async function updateTask(id, status) {
-    const result = await database.query(`
+  const result = await database.query(
+    `
         UPDATE tasks
         SET status = $2
         WHERE id = $1
         RETURNING *;
-    `, [id, status]);
-    console.log(result.rows[0]);
-    return result.rows[0];
+    `,
+    [id, status]
+  );
+  console.log(result.rows[0]);
+  return result.rows[0];
 }
 
-
 async function deleteTask(id) {
-    const result = await database.query(`
+  const result = await database.query(
+    `
         DELETE FROM tasks
         WHERE id = $1
         RETURNING *;
-    `, [id]);
-    console.log(result.rows[0]);
-    return result.rows[0];
+    `,
+    [id]
+  );
+  console.log(result.rows[0]);
+  return result.rows[0];
 }
 
 async function getUsersByHousehold(household_id) {
-    const result = await database.query(`
+  const result = await database.query(
+    `
     SELECT 
         * 
     FROM 
         users
     WHERE 
         household_id = $1;
-    `, [household_id]);
-    console.log(result.rows);
-    return result.rows;
+    `,
+    [household_id]
+  );
+  console.log(result.rows);
+  return result.rows;
 }
 
-
-
 module.exports = {
-    getTasksByHousehold,
-    getTasksByUser,
-    createTask,
-    updateTask,
-    deleteTask,
-    getUsersByHousehold
+  getTasksByHousehold,
+  getTasksByUser,
+  createTask,
+  updateTask,
+  deleteTask,
+  getUsersByHousehold,
 };
-
-
-
